47d0a5fa824d6a6c1446e21e40af098eff7fd94b
Nahum Orozco <nahumorozco@MacBook-Air-de-Nahum.local>
2026-02-18 14:39:04 -0700

    Fix syntax

--- app.py
+++ app.py
@@ -5,12 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"status": "FormatosFacilConta activo"})
-
-@app.route("/health")
-def health():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
